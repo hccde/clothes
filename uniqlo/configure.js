724c700361f07bc7ef8f8f24ea9da90d6673ce6d
hccde <1076663958@qq.com>
2017-07-05 13:15:08 +0800

    request queue

--- uniqlo/configure.js
+++ uniqlo/configure.js
@@ -2,16 +2,20 @@ let cheerio = require('cheerio');
 let utils = require('../lib/utils');
 let request = require('../lib/request');
 let globalConfig = require('../configure');
+
 let logFile = globalConfig.logFile;
-let warningLimit = 20,warningCount=0;
+let warningLimit = 20,warningCount=0,currentPage=1;
 module.exports = {
 	all:{
 		url:`http://www.uniqlo.cn/search.htm?search=y&viewType=grid&orderType=_newOn`,
 		method:'get',
 		params:{
-			pageNum:1
+			get pageNum(){
+				return currentPage++
+			}
 		},
 		totalPage:1,
+		currentPage:currentPage,
 		handle(str){
 			let $ =	cheerio.load(str);
 			let pageInfo  = $('.page-info').text().split('/');
@@ -23,21 +27,23 @@ module.exports = {
 				let goods = [];
 				try{
 					goods = $('.shop-list').find('li');
-					goodsData = goods.map((index, el) => {
+					goods.map((index, el) => {
 						let aele = $(el).find('.pic a')[0];
 						let desc = $(el).find('.desc')[0];
 						let price = $(el).find('.price strong')[0];
 						let saleAmount = $(el).find('.sales-amount em');
-						return {
+						let data = {
 							href: $(aele).attr('href'),
 							img: $(aele).find('img').attr('src'),
 							desc: $(desc).text().trim(),
 							price: $(price).text(),
 							sale: $(saleAmount).text()
 						}
+						goodsData.push(data);
+						return data;
 					})
-					console.log(goodsData);
-						//clear counter
+					console.log(goodsData)
+					//clear counter
 					warningCount = 0;
 				}catch(e){
 					logFile.warn('uniqlo page has changed,some class selector failed');
@@ -50,9 +56,10 @@ module.exports = {
 				}
 			}else{
 				request.kill('uniqlo');
+				return -1;
 			}
 			return goodsData;			
 		}
 	},
-	concurrency:20
+	concurrency:3
 }
--- uniqlo/index.js
+++ uniqlo/index.js
@@ -2,32 +2,43 @@ let config = require('./configure.js');
 let _ = require('lodash');
 let globalConfig = require('../configure.js');
 let logFile = globalConfig.logFile;
+let utils = require('../lib/utils');
+let requestFlag = true
+
 
 let request = require('../lib/request');
+let Event = require('../lib/event');
 let iconv = require('iconv-lite');
-let runTimeData = {
-	params:_.clone(config.all.params)
-}
-
+let startime = new Date();
 module.exports = {
 	run(){
-		//init runTimeData
-		runTimeData.params.pageNum = 1;
 		// request
-		this.req()
+		this.req();
+		Event.addEventListener('uniqloReq',()=>{
+			console.log('haha')
+			while(config.all.currentPage<config.all.totalPage && requestFlag){
+				this.req();
+				config.all.currentPage++;
+			}
+		})
 	},
 	req(){
 		let options = this.getOptions();
-		request.request(options,(error,res,body)=>{
+		let req = request.request(options,(error,res,body)=>{
 			if(error){
 				logFile.error(error);
 			}else{
 				let str = iconv.decode(body,'gbk');
 				//raw data
 				let goodsData = config.all.handle(str);
-				this.completeData(goodsData);
-				
+				if(goodsData == -1){
+					requestFlag = false;
+				}else{
+					this.completeData(goodsData);
+				}
 			}
+			request.remove(req);
+			Event.trigger('uniqloReq');
 		})
 	},
 	getOptions(){
@@ -35,7 +46,7 @@ module.exports = {
 			_name:'uniqlo',
 			method:'GET',
 			uri:config.all.url,
-			qs:runTimeData.params,
+			qs:config.all.params,
 			encoding:null
 		};
 		options = _.merge(options,globalConfig.headers);
