a263c393f5d66437292a0c098f911256f1eca629
hccde <1076663958@qq.com>
2017-07-17 10:27:43 +0800

    log and mail

--- configure.js
+++ configure.js
@@ -2,7 +2,11 @@ let log4js = require('log4js');
 log4js.configure({
   appenders: [
     { type: 'console' },
-    { type: 'file', filename: 'log.txt'}
+    { 	type: 'file', 
+    	filename: 'log.txt',
+    	naxLongSize:20480,
+    	backups:20
+	}
   ]
 });
 let logFile = log4js.getLogger('log');
