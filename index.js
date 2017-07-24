7831876294c80c8b9e5dc7dff8d1ae65eb369ada
hccde <1076663958@qq.com>
2017-07-24 18:14:23 +0800

    backup clear update tmp table

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ let hm = require('./hm');
 let zara = require('./zara');
 let vero = require('./veromoda');
 let only = require('./only');
+let utils = require('./lib/utils');
 // only.run();
 // vero.run();
 // zara.run();
 // hm.run();
 // uniqlo.run()
 
-setTimeout(()=>{vero.run()},0);
-setTimeout(()=>{hm.run()},0);
-setTimeout(()=>{zara.run()},0);
-setTimeout(()=>{only.run()},0);
-setTimeout(()=>{uniqlo.run()},0);
+// setTimeout(()=>{vero.run()},0);
+// setTimeout(()=>{hm.run()},0);
+// setTimeout(()=>{zara.run()},0);
+// setTimeout(()=>{only.run()},0);
+// setTimeout(()=>{uniqlo.run()},0);
+
+utils.updateDaily();
