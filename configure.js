cc87144fd8374451d8d712f2b146d30e145b71f5
hccde <1076663958@qq.com>
2017-11-16 13:32:30 +0800

    hm reconstruct

--- configure.js
+++ configure.js
@@ -1,18 +1,15 @@
-let log4js = require('log4js');
-log4js.configure({
-  appenders: [
-    { type: 'console' },
-    { 	type: 'file', 
-    	filename: 'log.txt',
-    	naxLongSize:20480,
-    	backups:20
-	}
-  ]
-});
-let logFile = log4js.getLogger('log');
-
-let _requestId = -1;
-
+// let log4js = require('log4js');
+// log4js.configure({
+//   appenders: [
+//     { type: 'console' },
+//     { 	type: 'file', 
+//     	filename: 'log.txt',
+//     	naxLongSize:20480,
+//     	backups:20
+// 	}
+//   ]
+// });
+// let logFile = log4js.getLogger('log');
  module.exports = {
  	headers:{
  		'User-Agent':`Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 
@@ -22,10 +19,5 @@ let _requestId = -1;
  		'Accept-Language': `zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4`,
  		'Cache-Control':`no-cache`,
  		'Connection':`keep-alive`,
- 	},
- 	logFile:logFile,
- 	website:['uniqlo','zara','hm','vero','only'],
- 	get requestId(){
- 		return _requestId+1;
  	}
  }
--- index.js
+++ index.js
@@ -18,26 +18,30 @@
 
 // utils.updateDaily();
 require('Promise');
-let Consumer  = require('./lib/interface/index');
+let Consumer  = require('./lib/pattern/index');
 
-let consumer  = new Consumer(6);
-let t = 0
-async function add(t){
-		console.log(t)
-		if(t>20){
-			await true;
-			return;
-		}
-		await consumer.push(()=>{
-			let time = 2000;
-			return new Promise((resolve,reject)=>{
-				setTimeout(()=>{
-					console.log('a ',time);
-					resolve();
-				},time)
-			})
-		});
-		t+=1
-		add(t);
-	}
-add(0);
+// let consumer  = new Consumer(6);
+// let t = 0
+// async function add(t){
+// 		console.log(t)
+// 		if(t>20){
+// 			await true;
+// 			return;
+// 		}
+// 		await consumer.push(()=>{
+// 			let time = 2000;
+// 			return new Promise((resolve,reject)=>{
+// 				setTimeout(()=>{
+// 					console.log('a ',time);
+// 					resolve();
+// 				},time)
+// 			})
+// 		});
+// 		t+=1
+// 		add(t);
+// 	}
+// add(0);
+
+let hm = require('./hm/_index.js');
+
+new hm().run();
