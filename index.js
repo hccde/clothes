a93399689a42f858fa7d3999124fd2a9dff027a6
hccde <1076663958@qq.com>
2017-11-24 16:16:23 +0800

    mail

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let Only = require('./shops/only.js');
 let Uniqlo = require('./shops/uniqlo.js');
 let Veromoda = require('./shops/veromoda.js');
 let Zara = require('./shops/zara.js');
-
+// const time = 12*3600*1000;
 setTimeout(()=>{
     new Hm().run();
 },0)
@@ -19,3 +19,4 @@ setTimeout(()=>{
 setTimeout(()=>{
     new Only().run();
 },0)
+
