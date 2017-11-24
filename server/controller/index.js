ef882d7f41fe2a7cdebb22f5f7cd4bd8b8bb4ea8
hccde <1076663958@qq.com>
2017-11-25 00:59:10 +0800

    rm files

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,11 +1,15 @@
+let logFile = require('../lib/log');
 let List = require('./list.controller');
 let History = require('./history.controller');
 
 function bootstrap(app){
 	app.get('/list', function (req, res) {
+		req.connection.remoteAddress // get ip todo
+		logFile.info(req.connection.remoteAddress)
 		List(req,res);
 	});
 	app.get('/history',function(req,res){
+		logFile.info('')
 		History(req,res);
 	})
 }
