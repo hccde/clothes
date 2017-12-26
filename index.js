a7a390684c531afbe727300e75fde60752e7e46e
hccde <1076663958@qq.com>
2017-12-26 15:31:47 +0800

    rm interal

--- index.js
+++ index.js
@@ -3,25 +3,22 @@ let Only = require('./shops/only.js');
 let Uniqlo = require('./shops/uniqlo.js');
 let Veromoda = require('./shops/veromoda.js');
 let Zara = require('./shops/zara.js');
-let time = 12*3600*1000;
+const time = 12*3600*1000;
 
-if(process.env.NODE_ENV == 'dev'){
-    time = 0;
+function run(){
+    return [Hm,Zara,Uniqlo,Veromoda,Only].map((item)=>{
+        if(process.env.NODE_ENV !== 'dev'){
+            setInterval(()=>{
+                new item().run();
+            },time);
+        }else{
+            setTimeout(()=>{
+                new item().run();
+            },0)
+        }
+        return item;
+    });
 }
 
-setInterval(()=>{
-    new Hm().run();
-},time)
-setInterval(()=>{
-    new Zara().run();
-},time)
-setInterval(()=>{
-    new Uniqlo().run();
-},time)
-setInterval(()=>{
-    new Veromoda().run();
-},time)
-setInterval(()=>{
-    new Only().run();
-},time)
+run();
 
