7e5be6dc711bf40d0a015174711c3f619b86dcd9
zakj <zakj@hancongcongdeMacBook-Pro.local>
2018-02-21 18:36:45 +0800

    go server

---
+++ https-server/src/src/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+    "fmt"
+)
+
+func main() {
+    r := gin.Default()
+    r.GET("/ping", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message": "pong",
+        })
+    })
+    fmt.Printf("server listen");
+    r.Run() // listen and serve on 0.0.0.0:8080
+
+}
