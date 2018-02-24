2e2c3a10630ee9e95c37d26d99a1601d40c9d513
zakj <zakj@hancongcongdeMacBook-Pro.local>
2018-02-24 20:24:39 +0800

    wetchat

--- wetchat/clothes/pages/index/index.js
+++ wetchat/clothes/pages/index/index.js
@@ -1,13 +1,16 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+var order = ['red', 'yellow', 'blue', 'green', 'red']
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    toView: 'red',
+    scrollTop: 100,
+    listData:new Array(100)
   },
   //事件处理函数
   bindViewTap: function() {
@@ -16,49 +19,70 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      wx.authorize({
-        scope: "scope.userInfo",
-        success: res => {
-          //success get access
-          console.log(res);
-        },
-        fail:res => {
-          console.log(res);
-        }
-      });
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    this.getAuth();
   },
   getUserInfo: function(e) {
-    console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+  },
+  getAuth:function(){
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: res => {
+              app.globalData.userInfo = res.userInfo;
+              this.setData({
+                userInfo: app.globalData.userInfo,
+                hasUserInfo: true
+              });
+            }
+          })
+        } else {
+          wx.authorize({
+            scope: "scope.userInfo",
+            success: res => {
+              console.log(res);
+              this.getAuth();
+            },
+            fail: res => {
+              console.log(res);
+            }
+          });
+
+        }
+      },
+      fail: res => {
+        console.log('get user info failed');
+      }
+    })
+  },
+  //scroll view
+  upper: function (e) {
+    console.log(e)
+  },
+  lower: function (e) {
+    console.log(e)
+  },
+  scroll: function (e) {
+    console.log(e)
+  },
+  tap: function (e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
+    }
+  },
+  tapMove: function (e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
     })
   }
 })
